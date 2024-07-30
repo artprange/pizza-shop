96d70207b5df6abd8e0a02cd3a468e2b5d4d8088
artprange <arthur.prange@gmail.com>
2024-07-30 18:07:33 -0300

    error page

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
+import { NotFound } from './pages/404'
 import { Dashboard } from './pages/app/dashboard/dasboard'
 import { Orders } from './pages/app/orders/orders'
 import { SingnIn } from './pages/auth/sign-in'
@@ -11,6 +12,8 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
+
     children: [
       { path: '/', element: <Dashboard /> },
       { path: '/orders', element: <Orders /> },
