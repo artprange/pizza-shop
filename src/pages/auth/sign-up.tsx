5c5dbe5782c0e4fb143e22647d86218f79e9939c
artprange <arthur.prange@gmail.com>
2024-07-31 15:29:54 -0300

    deployment fix

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -27,6 +27,7 @@ export function SingnUp() {
   } = useForm<SignUpForm>()
 
   async function handleSignUp(data: SignUpForm) {
+    console.log(data)
     try {
       await new Promise((resolve) => setTimeout(resolve, 2000))
 
