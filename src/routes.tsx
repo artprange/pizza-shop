b0e29b947da16c82cef27bbb23f871a066569770
artprange <arthur.prange@gmail.com>
2024-08-11 15:59:10 -0300

    errors and 404 page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,24 +7,40 @@ import { Dashboard } from './pages/app/dashboard/dasboard'
 import { Orders } from './pages/app/orders/orders'
 import { SingnIn } from './pages/auth/sign-in'
 import { SingnUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
-
+    errorElement: <Error />,
     children: [
-      { path: '/', element: <Dashboard /> },
-      { path: '/orders', element: <Orders /> },
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
     ],
   },
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SingnIn /> },
-      { path: '/sign-up', element: <SingnUp /> },
+      {
+        path: '/sign-in',
+        element: <SingnIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SingnUp />,
+      },
     ],
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
