86a06dff54111e6768213ff693a71b2a651b2db7
artprange <arthur.prange@gmail.com>
2024-08-11 15:51:50 -0300

    orderds loading

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -14,20 +14,19 @@ import {
 } from '../../../components/ui/table'
 import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
+import { OrderTableSkeleton } from './order-table-skeleton'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
-
   const orderId = searchParams.get('orderId')
   const customerName = searchParams.get('customerName')
   const status = searchParams.get('status')
-
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ['orders', pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -37,7 +36,6 @@ export function Orders() {
         status: status === 'all' ? null : status,
       }),
   })
-
   function handlePaginate(pageIndex: number) {
     setSearchParams((state) => {
       state.set('page', (pageIndex + 1).toString())
@@ -73,6 +71,8 @@ export function Orders() {
               </TableBody>
             </Table>
           </div>
+          {isLoadingOrders && <OrderTableSkeleton />}
+
           {result && (
             <Pagination
               onPageChange={handlePaginate}
